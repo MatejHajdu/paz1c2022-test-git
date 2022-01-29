351967c33902893e92a0faf31daee84a6b51a078
mtj.hjd <matohjd@gmail.com>
2022-01-29 17:41:34 +0100

    my name initials

--- src/com/matej/paz1c/First.java
+++ src/com/matej/paz1c/First.java
@@ -4,7 +4,9 @@ public class First {
 
     public static void main(String[] args) {
         System.out.println("M.H.");
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i:" + i);
+        }
 
 
 
